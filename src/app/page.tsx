856ad1f62cb004078b5488123a2eb3eede57be19
Tayyab Sabbir <100309512+T4yy4b@users.noreply.github.com>
2024-10-24 19:08:56 +0500

    moeww Changes...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import { messaging } from "@/firebase";
 import { getToken, onMessage } from "firebase/messaging";
 
-
 const PushNotification = () => {
   const [token, setToken] = useState<string | null>(null);
 
@@ -21,9 +20,7 @@ const PushNotification = () => {
             setToken(currentToken);
             console.log("FCM Token:", currentToken);
           } else {
-            console.error(
-              "No registration token available. Request permission to generate one."
-            );
+            console.error("No registration token available. Request permission to generate one.");
           }
         } catch (error) {
           console.error("Error getting FCM token:", error);
@@ -35,37 +32,50 @@ const PushNotification = () => {
 
     requestPermission();
 
-  
     onMessage(messaging, (payload) => {
       console.log("Message received in foreground: ", payload);
       const notificationTitle = payload.notification?.title || "No title";
       const notificationOptions = {
         body: payload.notification?.body || "No body",
-        icon: "/lifewwCloud.png" ,
-      
+        icon: "/lifewwCloud.png",
+        data: {
+          url: "https://www.weatherwalay.com/", // Replace with your desired URL
+        },
       };
 
-
       if (Notification.permission === "granted") {
-        new Notification(notificationTitle, notificationOptions);
+        const notification = new Notification(notificationTitle, notificationOptions);
         
+        // Handle notification click event
+        notification.onclick = (event) => {
+          event.preventDefault(); // Prevent the default behavior
+          window.open(notificationOptions.data.url, '_blank'); // Open the URL in a new tab
+        };
       }
     });
   }, []);
 
-
   const triggerNotification = async () => {
     if (token) {
       if (Notification.permission === "granted") {
-        new Notification("Push Notification Triggered", {
+        const notification = new Notification("Push Notification Triggered", {
           body: "A push notification should be triggered via Firebase",
-          icon: "/lifewwCloud.png", 
+          icon: "/lifewwCloud.png",
+          data: {
+            url: "https://www.weatherwalay.com/",
+          },
         });
+
+        // Handle notification click event
+        notification.onclick = (event) => {
+          event.preventDefault(); // Prevent the default behavior
+          window.open(notification.data.url, '_blank'); // Open the URL in a new tab
+        };
       } else {
         alert("User has not granted notification permission.");
       }
     } else {
-      alert("Allow notifications !");
+      alert("Allow notifications!");
     }
   };
 
