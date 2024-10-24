0d5d448c23ca44e0e19342b098fe4ae5a2f370f0
Tayyab Sabbir <100309512+T4yy4b@users.noreply.github.com>
2024-10-24 11:58:04 +0500

    more change

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getMessaging } from "firebase/messaging"; // Import Firebase Messaging
 
 // Your web app's Firebase configuration
